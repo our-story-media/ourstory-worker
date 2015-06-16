45707809c197e567cf5d9d2c3fd5583abe3e98c7
Tom Bartindale <tom@bartindale.com>
2015-06-16 16:53:51 +0100

    test

--- doedithandler.js
+++ doedithandler.js
@@ -74,7 +74,7 @@ module.exports = function(winston)
 
             var dir = path.normalize(path.dirname(require.main.filename) + uploaddir);
 
-            if (edit.media.length<2)
+            if (edit.media.length<2 || edit.media.length>6)
             {
                 logger.error("Less than 2 clips.");
                 callback('bury');
@@ -113,50 +113,6 @@ module.exports = function(winston)
                     });
                 });
 
-                _.each(edit.media,function(m){
-                    calls.push(function(cb){
-                        //return cb();
-                        var media = m;
-                        //download from s3
-                        //var ff = ffmpeg();
-                        logger.info("reading meta for "+path.normalize(dir+"/"+media.path.replace(config.S3_CLOUD_URL,'')));
-                        ffmpeg.ffprobe(path.normalize(dir+"/"+media.path.replace(config.S3_CLOUD_URL,'')), function(err, metadata) {
-                            //logger.error(err);
-                            //console.dir(metadata);
-                            media.meta = metadata;
-                            cb();
-                        });                    
-                    });
-                });
-
-
-
-                // calls.push(function(cb){
-                //     var json = {};
-                //     json.output = path.normalize(path.dirname(require.main.filename) + '/upload/' + edit.code + ".mp4");
-                //     json.files = [];
-                //     _.each(edit.media,function(m){
-                //         json.files.push({filename:path.normalize(dir+"/"+m.path.replace(config.S3_CLOUD_URL,''))});
-                //     });
-
-                //     var options = {
-                //       mode: 'text',
-                //       scriptPath:__dirname,
-                //       args: [JSON.stringify(json)]
-                //     };
-
-                //     //console.log(options.args);
-                     
-                //     PythonShell.run('./edit.py', options, function (err, results) {
-                //       if (err) throw err;
-                //       // results is an array consisting of messages collected during execution 
-                //       cb();
-                //       //console.log('results: %j', results);
-                //     });
-                // });
-
-
-                //_.each(edit.media,function(m){
                 calls.push(function(cb){
                     var mlt = new MLT
                       , multitrack = new MLT.Multitrack
@@ -176,8 +132,6 @@ module.exports = function(winston)
                         mlt.push(producer);
                         playlist.entry({
                             producer: producer,
-                            //startFrame:100,
-                            //length:m.meta.streams[0].nb_frames
                         });
                     });
 
@@ -192,43 +146,21 @@ module.exports = function(winston)
                         var videoFilename = path.normalize(path.dirname(require.main.filename) + '/upload/' +edit.code + ".mp4");
 
                         //TESTING:
-                        //foo.dv bar.dv -mix 25 -mixer luma -mixer mix:-1
                         var testcommand = [];
-                        // var paths = _.pluck(edit.media,function(m)
-                        // {
-                        //     //return path.normalize(dir+"/"+m.path.replace(config.S3_CLOUD_URL,''));
-                        //     //testcommand += " "++" -mix 25 -mixer luma ";
-                        // });
 
                         var paths = _.each(edit.media,function(m)
                         {
                             testcommand.push(path.normalize(dir+"/"+m.path.replace(config.S3_CLOUD_URL,'')));
-                            testcommand.push("-mix 25");
+                            testcommand.push("-mix 15");
                             testcommand.push("-mixer luma");
                         });
 
-                        //testcommand = paths.join(' -mix 25 -mixer luma ')
-
-                        //testcommand += " -mixer mix:-1";
-                        //var child = 'melt ' + mltFilename + ' -progress -consumer avformat:' + videoFilename + " strict=experimental";
-                        //var child = testcommand + ' -progress -consumer avformat:' + videoFilename + " strict=experimental";
                        testcommand.push('-progress');
-                       testcommand.push('-consumer avformat:' + videoFilename + " strict=experimental");
+                       testcommand.push('-consumer avformat:' + videoFilename + " strict=experimental -b 3000 -frag_duration 30");
 
                         logger.info('Melting. Please be Patient!');
                         
-                        // child = child_process.spawn(child, function (err, stdout, stderr) {
-                        //       if (err) {
-                        //         logger.error(err);
-                        //         return cb(err);
-                        //       }
-                        //       else
-                        //       {
-                        //         logger.info('Finished: ' + videoFilename);
-                        //         cb();
-                        //       }
-                        //    });
-                            console.log('melt ' + testcommand.join(' '));
+                         //   console.log('melt ' + testcommand.join(' '));
 
 
                             var exec = require('child_process').exec;
@@ -253,117 +185,9 @@ module.exports = function(winston)
                                 logger.info('closing code: ' + code);
                                 cb(code!=0);
                             });
-                            
-
-                            // ls.stdout.on('data', function (data) {
-                            //   logger.info('' + data);
-                            // });
-
-                            // ls.stderr.on('data', function (data) {
-                            //   logger.error('' + data);
-                            // });
-
-                            // ls.on('exit', function (code) {
-                            //     logger.info('Finished: ' + videoFilename);
-                            //   cb();
-                            // });
                         }
                     });
                 });
-                //});
-
-                //-c:v libx264
-                // _.each(edit.media,function(m){
-                //     calls.push(function(cb){
-                //         //return cb();
-                //         var media = m;
-                //         //download from s3
-                //         var ff = ffmpeg();
-                //         ff.input(path.normalize(dir+"/"+media.path.replace(config.S3_CLOUD_URL,'')));
-                //         ff.fps(30.333)
-                //         ff.videoCodec('libx264').outputOptions('-preset slower');
-                //         // ff.preset('slower');
-                //         ff.size('1920x?').aspect('16:9');
-                //         ff.outputOptions('-g 2')
-                //         ff.keepDAR();
-
-                //         ff.on('start',function(command){
-                //             console.log("ffmpeg "+command);
-                //         });
-                //         ff.on('error', function(err, stdout, stderr) {
-                //             //console.log(stderr);
-                //             //console.log(stdout);
-                //             logger.error('An error occurred: ' + err.message);
-                //             cb(true);
-                //           })
-                //           .on('end', function() {
-                //             logger.info('Conversion finished !');
-                //             cb();
-                //           })
-                //           .save(path.normalize(dir+"/"+media.path.replace(config.S3_CLOUD_URL,'')));
-                //     });
-                // });
-
-
-                //edit
-                // calls.push(function(cb){
-                //  //# this is a comment
-                //  // file '/path/to/file1'
-                //  // file '/path/to/file2'
-                //  // file '/path/to/file3'
-                //  var filelist = _.reduce(edit.media,function(all,m)
-                //  {
-                //      return all + "file " + m.path.replace(sails.config.S3_CLOUD_URL,'') + "\r\n";
-                //  },"");
-                //  //fs.writeFileSync(path.normalize(dir+"/" + edit.code + '.txt'),filelist);
-                //  cb();
-                // });
-
-                // calls.push(function(cb){
-                //     //return cb();
-                //     var ff = ffmpeg();
-                //     _.each(edit.media,function(m)
-                //     {
-                //         ff.mergeAdd(path.normalize(path.dirname(require.main.filename) + '/upload/' + m.path.replace(config.S3_CLOUD_URL,'')));
-                //     });
-
-                //     ff.on('start',function(command){
-                //         logger.info("ffmpeg "+command);
-                //     });
-                //     ff.on('error', function(err, stdout, stderr) {
-                //         //console.log(stderr);
-                //         //console.log(stdout);
-                //         logger.error('An error occurred: ' + err.message);
-                //         cb(true);
-                //       })
-                //       .on('end', function() {
-                //         logger.info('Merging finished !');
-                //         cb();
-                //       })
-                //       .mergeToFile(path.normalize(path.dirname(require.main.filename) + '/upload/' + edit.code + '.mp4'), path.normalize(path.dirname(require.main.filename) + '/.tmp/'));
-                // });
-
-                // calls.push(function(cb){
-
-                //  var ff = ffmpeg();
-                //  ff.addInput(path.normalize(path.dirname(require.main.filename) + '/upload/' + edit.code + '.mp4'));
-                //  ff.addInput(path.normalize(path.dirname(require.main.filename)+'/assets/images/logo.png'));
-                //  ff.complexFilter('overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2');
-                //  ff.output(path.normalize(path.dirname(require.main.filename) + '/upload/' + edit.code + '.mp4'));
-
-                //  ff.on('error', function(err) {
-                //      console.log('An error occurred: ' + err.message);
-                //    })
-                //    .on('end', function() {
-                //      console.log('Watermarking Finished!');
-                //      cb();
-                //    }).run();
-                      
-                // });
-
-
-                //ff.addInput(path.normalize(path.dirname(require.main.filename)+'/assets/images/logo.png'));
-                //ff.complexFilter('overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2');
 
                 //upload to s3
                 calls.push(function(cb){
