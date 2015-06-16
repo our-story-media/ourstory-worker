648a755fbdad17b88e6af0262a7ca781a4860755
Tom Bartindale <tom@bartindale.com>
2015-06-16 15:52:59 +0100

    test

--- doedithandler.js
+++ doedithandler.js
@@ -233,8 +233,8 @@ module.exports = function(winston)
 
                             var exec = require('child_process').exec;
                             //var ls = spawn('melt',testcommand,{stdio:[null,null,'pipe']});
-                            var child = exec('edit_do.sh "melt ' + testcommand.join(' ') + '"',function(err, o,e){
-                                
+                            var child = exec('./edit_do.sh "melt ' + testcommand.join(' ') + '"',function(err, o,e){
+
                             });
                             //logger.info(ls.stdout);
                             //var child = exec('node ./commands/server.js');
